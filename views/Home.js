6ce8a0fef64fbc6db9851c12c8c7e6be0fdd0e54
Mustafa Kemal Gordesli <m.kemalgordesli@gmail.com>
2024-02-28 16:53:03 +0300

    Add dynamic questions

--- views/Home.js
+++ views/Home.js
@@ -1,12 +1,50 @@
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Pressable } from 'react-native';
 import {
     useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import data from "../assets/tr.json"
+import { useState, useEffect } from "react"
 
 function Home() {
-    // console.log(data.words)
+    const generateThreeDistinctNumbers = (excludedNumber) => {
+        var numbers = [];
+
+        while (numbers.length < 3) {
+            var randomNumber = Math.floor(Math.random() * 1000) + 1;
+            if (randomNumber !== excludedNumber && !numbers.includes(randomNumber)) {
+                numbers.push(randomNumber);
+            }
+        }
+
+        return numbers;
+    }
+
+    const words = data.words
+
+    const getRandomQuest = () => {
+        SetCheckedOption(null)
+        const numb = Math.floor(Math.random() * 999)
+        SetQuest({
+            ...words[numb],
+        })
+        const arr = [...generateThreeDistinctNumbers(numb + 1), numb + 1]
+        SetOptions(arr.sort(() => Math.random() - 0.5))
+    }
+
+    const handleAnswer = (e) => {
+        SetCheckedOption(e)
+    }
+
     const insets = useSafeAreaInsets();
+    const [quest, SetQuest] = useState({})
+    const [options, SetOptions] = useState([])
+    const [checkedOption, SetCheckedOption] = useState(null)
+
+    useEffect(() => {
+        SetCheckedOption(null)
+        getRandomQuest()
+    }, [])
+
     return (
         <View
             style={{
@@ -20,16 +58,17 @@ function Home() {
             }}>
             <Text style={styles.text}>English-Turkish Word Quiz</Text>
             <View style={styles.quest}>
-                <View>
-
+                <View style={{ flex: 1, backgroundColor: "#d3d3d3", justifyContent: "center", alignItems: "center" }}>
+                    <Text style={{ textAlign: "center", fontSize: 24 }}>{quest.englishWord}</Text>
                 </View>
-                <View style={{ gap: 10 }}>
-                    <Button title="Learn More" style={styles.btn} />
-                    <Button title="Learn More" style={styles.btn} />
+                <View style={{ gap: 15, marginVertical: 20 }}>
+                    {
+                        options.map(item => <TouchableOpacity key={item} onPress={() => handleAnswer(item)} style={[styles.btn, checkedOption ? checkedOption == item ? item == quest?.rank ? styles.successBtn : styles.dangerBtn : "" : ""]}><Text style={{ fontSize: 18, color: "white" }}>{words[item - 1].targetWord}</Text></TouchableOpacity>)
+                    }
                 </View>
             </View>
-            <View style={{ flez: 1, alignItems: "end", width: "100%" }}>
-                <TouchableOpacity style={{ width: 160, paddingVertical: 6, marginLeft: "auto", marginRigth: 10, borderWidth: 2, borderRadius: 6 }}><Text style={{ fontSize: 18, textAlign: "center" }}>Next Question</Text></TouchableOpacity>
+            <View style={{ alignItems: "end", width: "100%", padding: 10 }}>
+                <TouchableOpacity onPress={getRandomQuest} style={{ width: 160, paddingVertical: 3, marginLeft: "auto", marginRigth: 10, borderWidth: 2, borderRadius: 6 }}><Text style={{ fontSize: 18, textAlign: "center" }}>Next Question</Text></TouchableOpacity>
             </View>
         </View >
     )
@@ -38,6 +77,7 @@ function Home() {
 const styles = StyleSheet.create({
     text: {
         fontSize: 25,
+        paddingVertical: 10,
         fontWeight: '500',
     },
     quest: {
@@ -48,8 +88,18 @@ const styles = StyleSheet.create({
         marginVertical: 20,
     },
     btn: {
-        marginHorizontal: 20,
-        padding: 30,
+        heigth: 240,
+        paddingVertical: 5,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#48A8ED",
+        borderRadius: 2,
+    },
+    dangerBtn: {
+        backgroundColor: "red"
+    },
+    successBtn: {
+        backgroundColor: "green"
     }
 });
 
